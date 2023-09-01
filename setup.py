01d1bfecfca882d2b6ea305ae99265ca44708234
GalaxySnail <me@glxys.nl>
2023-09-01 10:00:00 +0800

    将打包元数据迁移到 pyproject.toml

--- setup.py
+++ setup.py
@@ -1,48 +1,8 @@
-import os
 from setuptools import setup
 
-assert os.path.exists("pyproject.toml")
-
-# for __version__
-with open("src/yhdm_proxy/_version.py", encoding="utf-8") as f:
-    exec(f.read())  # pylint: disable=W0122
-
-with open("README.md", encoding="utf-8") as f:
-    readme = f.read()
-
-
+# Home-page is not supported in PEP 621, which causes some issues.
+# https://github.com/pypa/packaging-problems/issues/606
+# https://github.com/pypa/pip/issues/11221
 setup(
-    name="yhdm-proxy",
-    version=__version__,  # type: ignore[name-defined]  # pylint: disable=E0602
     url="https://github.com/GalaxySnail/yhdm_proxy",
-    author="GalaxySnail",
-    description="一个用于下载樱花动漫 m3u8 文件的反向代理",
-    long_description=readme,
-    license="AGPL-3.0-or-later",
-    package_dir={"": "src"},
-    install_requires=[
-        "trio >= 0.20.0",
-        "httpx >= 0.24.0",
-        "h11 >= 0.13.0",
-    ],
-    package_data={"yhdm_proxy": ["py.typed"]},
-    python_requires=">=3.7",
-    keywords=["trio", "proxy", "樱花动漫", "m3u8", "png"],
-    classifiers = [
-        "Development Status :: 4 - Beta",
-        "Framework :: Trio",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-        "Natural Language :: Chinese (Simplified)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Topic :: Internet :: Proxy Servers",
-        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
-        "Topic :: Multimedia :: Video :: Conversion",
-        "Typing :: Typed",
-    ],
 )
